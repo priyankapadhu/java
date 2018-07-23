8b33545f9f4911ae9a9e86eca0ae0bd1fafe1417
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-07-23 09:59:12 +0530

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner.*;
+class ex1
+{
+ 
+ public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input your first name: ");
+    String fname = input.next();
+    System.out.print("Input your last name: ");
+    String lname = input.next();
+    System.out.println();
+    System.out.println("Hello \n"+fname+" "+lname);
+  }
+}
+ 

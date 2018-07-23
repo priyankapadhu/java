dbf7bbd359b8f8b3d8d1e87fdf94d735aaa8fa1b
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-07-23 09:38:51 +0530

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+class ex9
+{
+ 
+    public static void main(String[] arg) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
